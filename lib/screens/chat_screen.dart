b4ec2c82aa10312d2c9e11ab7f37f1a14a58b167
Hichem <hichemfredj123@gmail.com>
2021-05-05 00:01:14 -0400

    - Listening to chat message - Sending & ordering chat messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:first_app/widgets/chat/messages.dart';
+import 'package:first_app/widgets/chat/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -33,36 +35,18 @@ class ChatScreen extends StatelessWidget {
                 FirebaseAuth.instance.signOut();
               }
             },
-          )
+          ),
         ],
       ),
-      body: StreamBuilder(
-        stream: Firestore.instance
-            .collection('chats/pGnQsCNKQOIURKrLjjRv/messages/')
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          final documents = streamSnapshot.data.documents;
-          return ListView.builder(
-            itemCount: documents.length,
-            itemBuilder: (ctx, index) => Container(
-              padding: EdgeInsets.all(8),
-              child: Text(documents[index]['text']),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Messages(),
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Firestore.instance
-              .collection('chats/pGnQsCNKQOIURKrLjjRv/messages/')
-              .add({'text': 'This was added by clicking the button'});
-        },
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }
